1ef5d9ee79da8614d34388a58e8b5f3444c9e275
Huangchenghao <chym123@vip.qq.com>
2018-09-30 10:17:58 +0800

    nothing

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Progress } from 'antd';
 import autobind from 'autobind-decorator';
 import { connect } from 'react-redux';
 import action from '@/redux/home/action';
@@ -15,6 +15,10 @@ import Counter from '@/component/Counter';
 )
 export default class Home extends Component {
 
+    state = {
+        percent: 0,
+    }
+
     @autobind
     handleSetImmer() {
         this.props.setImmer();
@@ -24,10 +28,33 @@ export default class Home extends Component {
         const name = this.props.home.name;
         return <div>
             <p>{ name && `Hello, ${name}!` }</p>
+            <Progress type="circle" percent={this.state.percent} />
+            <Button.Group>
+                <Button onClick={this.decline} icon="minus" />
+                <Button onClick={this.increase} icon="plus" />
+            </Button.Group>
             <Button type="primary" onClick={this.handleSetImmer}>Immer Test</Button>
             <Counter></Counter>
         </div>;
         
+    }
+
+    @autobind
+    increase() {
+        let percent = this.state.percent + 10;
+        if (percent > 100) {
+            percent = 100;
+        }
+        this.setState({ percent });
+    }
+    
+    @autobind
+    decline() {
+        let percent = this.state.percent - 10;
+        if (percent < 0) {
+            percent = 0;
+        }
+        this.setState({ percent });
     }
 }
 
